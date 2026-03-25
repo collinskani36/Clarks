ec2ee38b40584ed31877c557c38ba85b84790898
collinskani36 <collinskani36@gmail.com>
2026-03-25 17:16:50 +0300

    update admin

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
-import AdminPage from "./pages/AdmiPage.tsx";
+import AdminPage from "./pages/AdminPage.tsx";
 
 const queryClient = new QueryClient();
 
